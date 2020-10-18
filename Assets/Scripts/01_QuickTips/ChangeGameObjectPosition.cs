6526391f31d8786f02b72621e675fba427816b56
garlicsuter <markasuter@gmail.com>
2020-10-18 19:53:14 -0400

    01 Quick Tips: Set object position complete

---
+++ Assets/Scripts/01_QuickTips/ChangeGameObjectPosition.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeGameObjectPosition : MonoBehaviour
+{
+    // Method 1: assign the new position in the Inspector in Unity by making a PUBLIC variable
+    // (private ones won't show in the Inspector!)
+    public Vector3 startPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Method 1: grab the existing position and assign it to the X,Y,Z you set in the Unity Inspector window
+        transform.position = startPosition;
+
+        // Method 2: assign at the Start. Grab the existing position and assign (=) it a 
+        // new position of zero X, zero Y, and zero Z.
+        // THE LINE BELOW IS COMMENTED BECAUSE WE ARE USING METHOD 1 INSTEAD.
+
+        // transform.position = new Vector3(0, 0, 0);
+
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
